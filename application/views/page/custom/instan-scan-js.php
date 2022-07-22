6162544d53e27d07476211a1745103dff1bbaffb
Gading Sobad <gading.rengga@gmail.com>
2022-07-22 20:35:27 +0700

    Test

--- application/views/page/custom/instan-scan-js.php
+++ application/views/page/custom/instan-scan-js.php
@@ -19,7 +19,7 @@
     <div class="row align-items-center">
         <div class="col text-center p-5">
             <h1 class="bold mb-5">Scan Barcode Disini</h1>
-            <video style="width:100%" class="rounded" id="preview"></video>
+            <video autoplay style="width:100%" class="rounded" id="preview"></video>
         </div>
         <div class="col p-5 bg-light-primary">
             <div id="title-form" class="text-center pb-4">
