97e501962743e08641c9a7bb9b3100caa019bdd4
Gading Sobad <gading.rengga@gmail.com>
2022-07-23 10:22:18 +0700

    Test

--- application/views/page/custom/scan-package.php
+++ application/views/page/custom/scan-package.php
@@ -19,7 +19,7 @@
     <div class="row align-items-center">
         <div class="col text-center p-5">
             <h1 class="bold">Scan Barcode Disini</h1>
-            <video autoplay style="width:100%;transform: scaleX(-1);" class="rounded" id="preview"></video>
+            <video autoplay style="width:100%;transform:rotate(180deg);" class="rounded" id="preview"></video>
         </div>
         <div class="col p-5 bg-light-primary">
             <div id="title-form" class="text-center pb-4">
