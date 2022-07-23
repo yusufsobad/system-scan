0c86d00d4a7c4709f21b7c83d326708b7f0a3d42
Gading Sobad <gading.rengga@gmail.com>
2022-07-23 09:51:08 +0700

    Update Scan android

--- application/views/page/custom/scan-package.php
+++ application/views/page/custom/scan-package.php
@@ -2,25 +2,42 @@
 <html lang="en">
 
 <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>vendor/bootstrap-4/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>vendor/bootstrap-4/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>vendor/bootstrap-4/css/bootstrap.min.css">
+
     <title>Solo Abadi</title>
-    <link rel="stylesheet" href="<?= base_url('assets/') ?>custom/css/scan-package.css">
 </head>
 
 <body>
 
-    <video style="width:100%" id="preview"></video>
+    <div class="row align-items-center">
+        <div class="col text-center p-5">
+            <h1 class="bold mb-5">Scan Barcode Disini</h1>
+            <video autoplay style="width:100%" class="rounded" id="preview"></video>
+        </div>
+        <div class="col p-5 bg-light-primary">
+            <div id="title-form" class="text-center pb-4">
+                <h1>Input Data Qrcode</h1>
+            </div>
+            <div class="card rounded shadow p-3 mb-5 bg-white rounded" id="form_scan" style="min-height: 326px;">
 
-    <div id="title-form" class="text-center pb-4">
-        <h1>Input Data Qrcode</h1>
-    </div>
-    <div class="card rounded shadow p-3 mb-5 bg-white rounded" id="form_scan" style="min-height: 326px;">
+            </div>
+        </div>
     </div>
     <div id="destinate"></div>
 
 
+    <!-- Optional JavaScript; choose one of the two! -->
+    <!-- Instan-Scan -->
     <script script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="<?= base_url('assets/plugin/') ?>instascan/js/instascan.min.js"></script>
-    <!-- <script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script> -->
+
     <script>
         document.addEventListener("DOMContentLoaded", event => {
             let scanner = new Instascan.Scanner({
@@ -87,6 +104,13 @@
         });
     </script>
 
+
+
+
+
+    <!-- Bootstrap - Js -->
+    <script src="<?= base_url('assets') ?>/vendor/bootstrap-4/js/bootstrap.min.js"></script>
+    <script src="<?= base_url('assets') ?>/vendor/bootstrap-4/js/bootstrap.bundle.min.js"></script>
 
 </body>
 
