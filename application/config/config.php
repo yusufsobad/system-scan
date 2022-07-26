8563179b309e4210fe7ba95df623caded5be964f
Gading Sobad <gading.rengga@gmail.com>
2022-07-26 15:39:58 +0700

    Update config and db

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'https://s.soloabadi.com/scan-package/';
-$config['base_url'] = 'http://localhost/system-scan/';
+$config['base_url'] = 'https://s.soloabadi.com/scan-package/';
+// $config['base_url'] = 'http://localhost/system-scan/';
 
 /*
 |--------------------------------------------------------------------------
