7eb486653e85d6c534407235b29ee96ac1a69580
Gading Sobad <gading.rengga@gmail.com>
2022-08-10 11:29:07 +0700

    Update

--- application/views/page/custom/report-packing.php
+++ application/views/page/custom/report-packing.php
@@ -14,7 +14,7 @@ $no = 0;
         font-family: sans-serif;
         min-width: 400px;
         box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
-        page-break-inside: avoid
+        overflow: hidden;
     }
 
     .styled-table thead tr {
