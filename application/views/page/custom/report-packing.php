8873b97b38850674cdd8b61c2105b1d37bcb1382
Gading Sobad <gading.rengga@gmail.com>
2022-08-12 13:57:11 +0700

    Update css pdf

--- application/views/page/custom/report-packing.php
+++ application/views/page/custom/report-packing.php
@@ -9,18 +9,14 @@ $no = 0;
 
     .styled-table {
         border-collapse: collapse;
-        margin: 25px 0;
         font-size: 0.9em;
         font-family: sans-serif;
-        min-width: 400px;
         box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
 
     }
 
     table {
-        page-break-after: always;
         overflow: wrap;
-        min-width: 33%
     }
 
     .styled-table thead tr {
