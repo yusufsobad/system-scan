15cca8c5445475e56a765d67884aa1b0e66adbf5
yusufsobad <yusuf_soloabadi@yahoo.co.id>
2022-11-18 10:24:20 +0700

    fix init set

--- index.php
+++ index.php
@@ -54,6 +54,8 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
+ini_set("pcre.backtrack_limit", "5000000");
+
 define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 
 /*
